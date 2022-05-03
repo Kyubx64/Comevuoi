87f99f1348b4020e28f7d6822808cccbdca4a12c
Kyubx64 <gattinanera44@gmail.com>
2022-05-03 11:19:09 +0200

    es2

--- EserciziProgrammatori/Esercizio.sql
+++ EserciziProgrammatori/Esercizio.sql
@@ -6,3 +6,15 @@ select autore.codice
 from autore join programmatore on autore.codice = programmatore.codice
     join programma on programma.id = autore.id
 where programma.anno >= 2000 and programma.linguaggio = "Java"
+
+/*Query 2
+Calcolare il nome e la categoria dei programmatori
+che sono stati autori di almeno un programma scritto
+in un linguaggio diverso da Python, ordinando il
+risultato rispetto al nome dei programmatori. */
+
+select programmatore.nome, programmatore.categoria
+from autore join programmatore on autore.codice = programmatore.codice
+    join programma on programma.id = autore.id
+where programma.Linguaggio != "Python" 
+order by programmatore.nome
